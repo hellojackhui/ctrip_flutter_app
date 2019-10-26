dc4b8c85b796d1a52fe36515733125e9051a2851
hellojackhui <huijiawe.i@163.com>
2019-10-26 21:56:15 +0800

    travel page complete

--- lib/dao/travel_dao.dart
+++ lib/dao/travel_dao.dart
@@ -1,8 +1,11 @@
 import 'dart:async';
+
 import 'package:ctrip_flutter_app/model/travel_model.dart';
 import 'package:dio/dio.dart';
 
-const TRAVEL_URL = 'https://m.ctrip.com/restapi/soa2/16189/json/searchTripShootListForHomePageV2?_fxpcqlniredt=09031010211161114530&__gw_appid=99999999&__gw_ver=1.0&__gw_from=10650013707&__gw_platform=H5';
+const TRAVEL_URL =
+    'https://m.ctrip.com/restapi/soa2/16189/json/searchTripShootListForHomePageV2?_fxpcqlniredt=09031010211161114530&__gw_appid=99999999&__gw_ver=1.0&__gw_from=10650013707&__gw_platform=H5';
+
 var params = {
   "districtId": -1,
   "groupChannelCode": "tourphoto_global1",
@@ -31,9 +34,25 @@ var params = {
   },
   "contentType": "json"
 };
+
+///旅拍接口
 class TravelDao {
-  static Future<TravelModel> fetch(String url, Map params, String groupChannelCode, int type, int pageIndex, int pageSize) async {
+  static Future<TravelModel> fetch(
+    String url,
+    Map params,
+    String groupChannelCode,
+    int type,
+    int pageIndex,
+    int pageSize,
+  ) async {
+    Map paramsMap = params['pagePara'];
+    paramsMap['pageIndex'] = pageIndex;
+    paramsMap['pageSize'] = pageSize;
+    params['groupChannelCode'] = groupChannelCode;
+    params['type'] = type;
+
     Response response = await Dio().post(url, data: params);
+
     if (response.statusCode == 200) {
       return TravelModel.fromJson(response.data);
     } else {
--- lib/pages/travel_page.dart
+++ lib/pages/travel_page.dart
@@ -1,12 +1,78 @@
+import 'package:ctrip_flutter_app/dao/travel_tab_dao.dart';
+import 'package:ctrip_flutter_app/model/travel_tab_model.dart';
+import 'package:ctrip_flutter_app/pages/travel_tab_page.dart';
 import 'package:flutter/material.dart';
 
-class TravelPage extends StatelessWidget {
+class TravelPage extends StatefulWidget{
+  @override
+  _TravelPageState createState() => _TravelPageState();
+}
+
+class _TravelPageState extends State<TravelPage> with TickerProviderStateMixin{
+  TabController _controller;
+  List<TravelTab> tabs = [];
+  TravelTabModel travelTabModel;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    _controller = TabController(length: 0, vsync: this);
+    TravelTabDao.fetch().then((TravelTabModel model) {
+      _controller = TabController(vsync: this, length: model.tabs.length);
+      setState(() {
+        tabs = model.tabs;
+        travelTabModel = model;
+      });
+    }).catchError((e) {
+      print(e);
+    });
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    _controller.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Center(
-        child: Icon(Icons.camera_alt, size: 128.0,),
-      ),
+    return Column(
+      children: <Widget>[
+        Container(
+          color: Colors.white,
+          padding: EdgeInsets.only(top: 30),
+          child: TabBar(
+            controller: _controller,
+            isScrollable: true,
+            labelColor: Colors.black12,
+            labelPadding: EdgeInsets.fromLTRB(20, 0, 10, 5),
+            indicator: UnderlineTabIndicator(
+              borderSide: BorderSide(width: 3, color: Color(0xff2fcfbb)),
+              insets: EdgeInsets.fromLTRB(0, 0, 0, 10)
+            ),
+            tabs: tabs.map<Tab>((TravelTab tab) {
+              return Tab(
+                text: tab.labelName,
+              );
+            }).toList(),
+          ),
+        ),
+        Flexible(
+          child: TabBarView(
+            controller: _controller,
+            children: tabs.map((TravelTab tab) {
+              return TravelTabPage(
+                travelUrl: travelTabModel.url,
+                params: travelTabModel.params,
+                groupChannelCode: tab.groupChannelCode,
+                type: tab.type,
+              );
+            }).toList(),
+          ),
+        )
+      ],
     );
   }
 }
